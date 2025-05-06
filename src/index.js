8c708ac49d7134fb26511e22f9b3d425ced83dfa
Hlib Nikitin <gleb.nikitin18@gmail.com>
2025-05-06 18:18:03 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/contacts.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
